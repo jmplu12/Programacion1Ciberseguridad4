c70ae4754ee77c4e17b2628006535ca6eb410021
Jose Florian <joseflorian2025@gmail.com>
2025-11-21 10:46:27 -0400

    cambios

---
+++ Tipo de Dato: EnteroPython/SoliciteDosNumeros.py
@@ -0,0 +1,5 @@
+#1.1. Solicite al usuario dos números enteros y muestre su suma.
+num1 = int(input("Ingrese el primer número entero: "))
+num2 = int(input("Ingrese el segundo número entero: "))
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es:", suma)
